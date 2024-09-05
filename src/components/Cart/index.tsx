f9b0414364337beabb2f4e46bdddc34540affa98
Kayky Dion <kayky.dion@gmail.com>
2024-09-05 09:28:33 -0300

    create empty cart message and cart opening animation

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,6 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "../../styles";
-import { CartContainer, CartItem, OverLay, SideBar } from "./styles";
+import {
+  CartContainer,
+  CartItem,
+  EmptyCartMessage,
+  OverLay,
+  SideBar,
+} from "./styles";
 import { close, remove } from "../../store/reducers/cart";
 import { RootReducer } from "../../store";
 import formatPrice from "../../utils/formatPrice";
@@ -29,22 +35,30 @@ export default function Cart() {
   return (
     <CartContainer className={isOpen ? "is-open" : ""}>
       <SideBar>
-        <ul>
-          {items.map((item) => (
-            <CartItem key={generateRandomKey()}>
-              <img src={item.foto} />
-              <div>
-                <h3>{item.nome}</h3>
-                <span>{formatPrice(item.preco)}</span>
-              </div>
-              <button onClick={() => removeProduct(item)} />
-            </CartItem>
-          ))}
-        </ul>
-        <p>
-          Valor total: <span>{getTotalPrice()}</span>
-        </p>
-        <Button>Continuar com a entrega</Button>
+        {items.length ? (
+          <>
+            <ul>
+              {items.map((item) => (
+                <CartItem key={generateRandomKey()}>
+                  <img src={item.foto} />
+                  <div>
+                    <h3>{item.nome}</h3>
+                    <span>{formatPrice(item.preco)}</span>
+                  </div>
+                  <button onClick={() => removeProduct(item)} />
+                </CartItem>
+              ))}
+            </ul>
+            <p>
+              Valor total: <span>{getTotalPrice()}</span>
+            </p>
+            <Button>Continuar com a entrega</Button>
+          </>
+        ) : (
+          <EmptyCartMessage>
+            <p>Não há produtos no carrinho</p>
+          </EmptyCartMessage>
+        )}
       </SideBar>
       <OverLay onClick={closeCart}></OverLay>
     </CartContainer>
